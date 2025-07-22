7d13f67f0166e654fbba257d0ced216321474521
AVARELOWM <93490734+avarelowm2021@users.noreply.github.com>
2025-07-22 15:15:12 +0200

    Update http.js

--- examples/http.js
+++ examples/http.js
@@ -1,34 +1,28 @@
-;(function(){
-	var cluster = require('cluster');
-	if(cluster.isMaster){
-	  return cluster.fork() && cluster.on('exit',function(){ cluster.fork(); require('../lib/crashed') });
-	}
+const Gun = require('gun');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = process.env.PORT || 3000;
 
-	var fs = require('fs'), env = process.env;
-	var GUN = require('../'); // require('gun');
-	var opt = {
-		port: env.PORT || process.argv[2] || 8765,
-		peers: env.PEERS && env.PEERS.split(',') || []
-	};
+// Créer un serveur HTTP simple
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Erreur serveur');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  }
+});
 
-	if(fs.existsSync((opt.home = require('os').homedir())+'/cert.pem')){
-		env.HTTPS_KEY = env.HTTPS_KEY || opt.home+'/key.pem';
-		env.HTTPS_CERT = env.HTTPS_CERT || opt.home+'/cert.pem';
-	}
-	if(env.HTTPS_KEY){
-		opt.port = 443;
-		opt.key = fs.readFileSync(env.HTTPS_KEY);
-		opt.cert = fs.readFileSync(env.HTTPS_CERT);
-		opt.server = require('https').createServer(opt, GUN.serve(__dirname));
-		require('http').createServer(function(req, res){
-			res.writeHead(301, {"Location": "https://"+req.headers['host']+req.url });
-			res.end();
-		}).listen(80);
-	} else {
-		opt.server = require('http').createServer(GUN.serve(__dirname));
-	}
+// Ajouter GUN au serveur
+Gun({ web: server });
 
-	var gun = GUN({web: opt.server.listen(opt.port), peers: opt.peers});
-	console.log('Relay peer started on port ' + opt.port + ' with /gun');
-	module.exports = gun;
-}());
+server.listen(port, () => {
+  console.log(`🟢 Gun relay running on port ${port}`);
+});
